f58d1c23d1f2ec68075f6e49f1ad9836954e074a
Seongjun2 <tjdwns4302@naver.com>
2019-08-19 21:56:37 +0900

    index페이지 test

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <v-app>
+    <div>
+    <img id = "logo" src="~/assets/Logo_namedYule.png" v-on:click="go_home" />
+    </div>
+    <nav>
+      <v-toolbar dark color="blue">
+        <v-spacer>
+        </v-spacer>
+        <v-toolbar-items>
+          <v-text-field label = "검색" hide-details prepend-icon="mdi-magnify" :style="{ display: 'flex', alignItems: 'center' }"/>
+          <v-btn text nuxt to = "/profile" :style="{ display: 'flex', alignItems:'center' }">
+            <div>재고 확인</div>
+          </v-btn>
+          <v-btn text nuxt to = "/signup">
+            <div>재고 검색</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </nav>
+    <nuxt/>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  #logo{
+    display: flex;
+    align-items: center;
+    left : 50%;
+    transform: translate(-50%, 0);
+    height:6em;
+    position: relative;
+  }
+</style>
